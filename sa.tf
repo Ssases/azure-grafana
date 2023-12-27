6bc697ac660752fc8437e1561513beb647935607
Siva <shankar.civil.34@gmail.com>
2023-12-27 15:37:09 +0000

    Test

--- sa.tf
+++ sa.tf
@@ -4,7 +4,7 @@ resource "terraform_data" "grafana-sa" {
   provisioner "local-exec" {
     command = <<EOF
     az grafana service-account create -g My-app -n grafana-testing --service-account myAccount --role admin
-    TOKEN=$(az grafana service-account token create -g My-app -n grafana-testing --service-account myAccount --token myToken --time-to-live 60s --query key  -o tsv)
+    TOKEN=$(az grafana service-account token create -g My-app -n grafana-testing --service-account myAccount --token myToken --time-to-live 1d --query key  -o tsv)
     echo -n $TOKEN >> grafana_token.txt
     EOF
 
