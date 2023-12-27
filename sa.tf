c11401b7fc4bbe4434f16087a85b26abe9908a8b
Siva <shankar.civil.34@gmail.com>
2023-12-27 04:33:09 +0000

    test

---
+++ sa.tf
@@ -0,0 +1,21 @@
+resource "terraform_data" "grafana-sa" {
+  triggers_replace = timestamp()
+
+  provisioner "local-exec" {
+    command = <<EOF
+    az grafana service-account create -g My-app -n grafana-testing --service-account myAccount --role admin
+    TOKEN=$(az grafana service-account token create -g My-app -n grafana-testing --service-account myAccount --token myToken --time-to-live 60s --query -o tsv)
+    echo -n $TOKEN >> grafana_token.txt
+    EOF
+
+    interpreter = [ "bash", "-c" ]
+  }
+}
+
+data "local_file" "grafana_token" {
+  filename = "${path.module}/grafana_token.txt"
+}
+
+output "grafana_token" {
+  value = data.local_file.grafana_token.content
+}
